42292a33e06c1e0924fec7ca387002369a106258
Jaakko Utriainen <jaakko.utriainen@tuni.fi>
2024-10-18 16:22:48 +0300

    added a class to hold data from WhereISSAPI

---
+++ src/main/java/ryhma5/model/ISSInfo.java
@@ -0,0 +1,70 @@
+package ryhma5.model;
+
+public class ISSInfo {
+    private String name;
+    private int id;
+    private double latitude;
+    private double longitude;
+    private double altitude;
+    private double velocity;
+    private String visibility;
+    private double footprint;
+    private long timestamp;
+    private double daynum;
+    private double solar_lat;
+    private double solar_lon;
+    private String units;
+
+    // Getters for the fields
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public double getVelocity() {
+        return velocity;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+
+    public double getFootprint() {
+        return footprint;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public double getDaynum() {
+        return daynum;
+    }
+
+    public double getSolar_lat() {
+        return solar_lat;
+    }
+
+    public double getSolar_lon() {
+        return solar_lon;
+    }
+
+    public String getUnits() {
+        return units;
+    }
+}
