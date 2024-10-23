6e2352c0b94d9507d35ff4fa8bef85772524b9fb
Jaakko Utriainen <jaakko.utriainen@tuni.fi>
2024-10-23 14:34:20 +0300

    added API call to get ISS positions as a list

--- src/main/java/ryhma5/viewmodel/WhereISSViewModel.java
+++ src/main/java/ryhma5/viewmodel/WhereISSViewModel.java
@@ -3,12 +3,23 @@ package ryhma5.viewmodel;
 import ryhma5.model.ISSResponse;
 import ryhma5.model.WhereISSAPI;
 
+import java.util.List;
+
 public class WhereISSViewModel {
     public ISSResponse getISS(String units, Long timestamp) {
         try {
             return WhereISSAPI.fetchISS(units, timestamp);
         } catch (Exception e) {
             System.err.println("Error fetching ISS information: " + e.getMessage());
+            return null; // or throw a custom exception if needed
+        }
+    }
+
+    public List<ISSResponse> getISSPositions(List<Long> timestamps, String units) {
+        try {
+            return WhereISSAPI.fetchISSPositions(timestamps, units);
+        } catch (Exception e) {
+            System.err.println("Error fetching ISS positions: " + e.getMessage());
             return null; // or throw a custom exception if needed
         }
     }
