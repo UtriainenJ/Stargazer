d6c27e123c3e53324267dc24168383fa61782f57
soronen <89025008+soronen@users.noreply.github.com>
2024-11-27 14:18:15 +0200

    remove unecessary timer in starchartproxy

--- src/main/java/ryhma5/model/api/astronomyAPI/StarChartProxy.java
+++ src/main/java/ryhma5/model/api/astronomyAPI/StarChartProxy.java
@@ -8,6 +8,7 @@ import ryhma5.controller.AstronomyController;
 import java.time.format.DateTimeFormatter;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.CompletableFuture;
 
 public class StarChartProxy {
     private final ImageView imageView;
@@ -15,7 +16,6 @@ public class StarChartProxy {
     private final AstronomyResponse response;
     private final Image placeholderImage;
     private final Image failedImage;
-    private static final int TIMEOUT_MS = 30000;
 
     public StarChartProxy(ImageView imageView, AstronomyController astronomyController, AstronomyResponse response) {
         this.imageView = imageView;
@@ -32,7 +32,6 @@ public class StarChartProxy {
     }
 
     private void loadStarChartImageAsync() {
-        Timer timeoutTimer = new Timer();
         double latitude = response.getObserverLatitude();
         double longitude = response.getObserverLongitude();
         String date = response.getDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
@@ -40,6 +39,7 @@ public class StarChartProxy {
 
         // create new thread to avoid blocking the GUI
         new Thread(() -> {
+            System.out.println("sending out the request");
             String starChartUrl = astronomyController.getConstellationStarChart(latitude, longitude, date, constellation);
             System.out.println("starcharturl " + starChartUrl);
 
@@ -51,22 +51,9 @@ public class StarChartProxy {
             Image actualImage = new Image(starChartUrl, true);
             actualImage.progressProperty().addListener((obs, oldProgress, newProgress) -> {
                 if (newProgress.doubleValue() >= 1.0) {
-                    timeoutTimer.cancel();
                     Platform.runLater(() -> imageView.setImage(actualImage));
                 }
             });
         }).start();
-
-        // Schedule a timeout to set the failed image if loading takes too long
-        timeoutTimer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Platform.runLater(() -> {
-                    if (imageView.getImage() == placeholderImage) {
-                        imageView.setImage(failedImage);
-                    }
-                });
-            }
-        }, TIMEOUT_MS);
     }
 }
